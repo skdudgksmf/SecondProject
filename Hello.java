6bfeabc2182fea7edbb80143e012d324a213e4ba
skdudgksmf <skdudgksmf@naver.com>
2015-12-29 09:26:55 +0900

    GitHub for git pull

--- Hello.java
+++ Hello.java
@@ -7,6 +7,7 @@ public class Hello{
 		System.out.println("What is Git?");
                 System.out.println("That's DVCS");
                 System.out.println("2002 BitKeeper");
+                System.out.println("That's version control");
 
 	}
 }
